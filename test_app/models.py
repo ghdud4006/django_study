a34d426f2000f30ea69556c773bfb73a2c0aead5
ghdud4006 <ghdud4006@gmail.com>
2018-01-25 22:26:23 +0900

    Clear DB

--- test_app/models.py
+++ test_app/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+from django.utils import timezone
+
+class testmodel(models.Model):
+    model_ID = models.CharField(max_length=12)
+    model_NAME = models.CharField(max_length=20)
+    model_PASSWORD = models.CharField(max_length=32)
+    
