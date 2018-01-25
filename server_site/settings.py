9057f2ad3f48df03ed6ed6d5e4efcef8107f5a88
ghdud4006 <ghdud4006@gmail.com>
2018-01-25 20:56:59 +0900

    Update database -> MySQL

--- server_site/settings.py
+++ server_site/settings.py
@@ -81,8 +81,8 @@ WSGI_APPLICATION = 'server_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tester', # DB명
-        'USER': 'root', # 데이터베이스 계정
+        'NAME': 'testdb', # DB명
+        'USER': 'testuser', # 데이터베이스 계정
         'PASSWORD': '1234', # 계정 비밀번호
         'HOST': 'localhost', # 데이테베이스 주소(IP)
         'PORT': '3306', # 데이터베이스 포트(보통은 3306)
--- test_app/models.py
+++ test_app/models.py
@@ -1,4 +1,1 @@
 from django.db import models
-
-class tester(models.Model):
-    text = models.TextField()
